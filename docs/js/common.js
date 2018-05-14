68ff74c533671dad8f5def4a98df9d52366903f2
Elenhtml <33184613+Elenhtml@users.noreply.github.com>
2018-05-14 16:04:50 +0300

    Add files via upload

---
+++ docs/js/common.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var navMain = document.querySelector(".main-nav");
+var navButtonMenu = document.querySelector(".button-menu--top");
+
+navMain.classList.remove("main-nav--nojs");
+navButtonMenu.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
+
+var navService = document.querySelector(".services-nav");
+var navServiceMenu = document.querySelector(".button-menu--middle");
+
+navService.classList.remove("services-nav--nojs");
+navServiceMenu.addEventListener("click", function () {
+  if (navService.classList.contains("services-nav--closed")) {
+    navService.classList.remove("services-nav--closed");
+    navService.classList.add("services-nav--opened");
+  } else {
+    navService.classList.add("services-nav--closed");
+    navService.classList.remove("services-nav--opened");
+  }
+});
+
+var testButton = document.querySelector(".form__button");
+if (testButton) {
+  testButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    swal("Ваша заявка отправлена!");
+  });
+}
